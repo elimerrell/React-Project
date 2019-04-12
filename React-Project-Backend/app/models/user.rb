9470dac1e174ec50a016f2857f01a763c3597178
omfgtora <spaminol@gmail.com>
2019-04-12 10:07:17 -0700

    Setup JWT

--- React-Project-Backend/app/models/user.rb
+++ React-Project-Backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :user_groups
-    has_many :notes 
+    has_many :notes
 end
--- React-Project-Backend/db/schema.rb
+++ React-Project-Backend/db/schema.rb
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2019_04_11_001307) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
