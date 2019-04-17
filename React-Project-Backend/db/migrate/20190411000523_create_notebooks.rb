1476aa245da9c35612a6d6df7af118da5033569a
omfgtora <spaminol@gmail.com>
2019-04-17 09:18:35 -0700

    Fixed typo in migration. Added user seed.

--- React-Project-Backend/db/migrate/20190411000523_create_notebooks.rb
+++ React-Project-Backend/db/migrate/20190411000523_create_notebooks.rb
@@ -2,7 +2,7 @@ class CreateNotebooks < ActiveRecord::Migration[5.2]
   def change
     create_table :notebooks do |t|
       t.integer :user_id
-      t.string: :category
+      t.string :category
       t.string :title
       t.string :description
       t.timestamps
